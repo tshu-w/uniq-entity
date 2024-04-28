d423cf51431273adb141f0f6042f49d27669a733
Tianshu Wang <wang@tianshu.me>
2024-04-28 02:54:15 +0000

    Add evaluation for NNBlocker

--- src/uniq_entity/blocker/nnblocker.py
+++ src/uniq_entity/blocker/nnblocker.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import pandas as pd
 from retriv import DenseRetriever, HybridRetriever, SparseRetriever
+from sklearn.metrics import auc
 
 
 class NNBlocker:
@@ -55,3 +56,49 @@ class NNBlocker:
         ]
 
         return self.batch_search(collection, topk=topk)
+
+    def evaluate(
+        self,
+        blocks: dict[dict],
+        matches: set[tuple],
+        *,
+        return_list: bool = False,
+    ) -> dict:
+        for k, v in blocks.items():
+            blocks[k] = sorted(v, key=v.get, reverse=True)
+
+        topk = max(len(dct) for dct in blocks.values())
+        candidates = []
+        flags = set()  # Comparison Propagation
+        for k in range(topk):
+            cands = set()
+            for i in blocks:
+                if k >= len(blocks[i]):
+                    continue
+
+                j = blocks[i][k]
+                pair = i, j
+                if i != j and pair not in flags:
+                    cands.add(pair)
+                    flags.add(pair)
+
+            candidates.append(cands)
+
+        cands = set()
+        precisions, recalls = [1], [0]
+        for k in range(topk):
+            cands |= candidates[k]
+            tp = len(cands & matches)
+            ppv = tp / len(cands)
+            tpr = tp / len(matches)
+            precisions.append(ppv)
+            recalls.append(tpr)
+
+        mAP = auc(recalls, precisions)
+        if not return_list:
+            precision = precisions[-1]
+            recall = recalls[-1]
+            f1 = 2 * (precision * recall) / (precision + recall)
+            return {"mAP": mAP, "Recall": recall, "Precision": precision, "F1": f1}
+        else:
+            return {"mAP": mAP, "Recalls": recalls, "Precisions": precisions}
